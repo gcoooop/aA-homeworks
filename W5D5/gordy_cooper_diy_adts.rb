e3e6a2318ad9817ff818dda79f0862af3a25a96b
Gordy Cooper <gcooper7@mail.csuchico.edu>
2019-03-14 20:57:59 -0700

    Created functionality for map class

--- W5D5/gordy_cooper_diy_adts.rb
+++ W5D5/gordy_cooper_diy_adts.rb
@@ -42,24 +42,43 @@ end
 class Map
 
     def initialize
-        
+        @map = []
     end
 
     def set(key, value)
+        
+        pair = [key, value]
 
+       (0...@map.length).each do |idx|
+            if @map[idx][0] == key
+                @map[idx] = pair
+                return true
+            end
+       end
+
+       @map << pair
+
+       @map << pair if @map.empty?
+       true
     end
 
     def get(key)
-
+        @map.each do |ele|
+            return ele[1] if ele[0] == key
+        end
+        nil
     end
 
     def delete(key)
-
+        @map.each do |ele|
+            @map.delete(ele) if ele[0] == key
+        end
     end
 
     def show
-
+        @map.each do |ele|
+            puts "#{ele[0]} => #{ele[1]}"
+        end
     end
 
 end
-
